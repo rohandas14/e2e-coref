08f44fff26c0785ba35999fed70324c3f5624137
rohandas14 <rohan.das@colorado.edu>
2023-01-10 11:20:26 -0700

    Adding early stopping.

--- preprocess.py
+++ preprocess.py
@@ -294,16 +294,21 @@ def get_document(doc_key, language, seg_len, tokenizer, udapi_document=None):
         word_feats = node.feats._string.split('|') if node.feats else []
         word_feats_onehot = []
         ud_features_dict = ud_features.get_ud_features_dict()
-        if node.upos is not None or node.upos != "":
-            if node.upos in ud_features_dict:
-                word_feats_onehot.append((ud_features_dict[node.upos]))
-            else:
-                print("No POS found with key: " + node.upos, flush=True)
         for feat in word_feats:
             try:
-                word_feats_onehot.append(ud_features_dict[feat])
+                if feat.startsWith("PronType"):
+                    prontypes = feat.split("=")[1].split(",")
+                    for prontype in prontypes:
+                        prontype_idx = "PronType=" + prontype
+                        word_feats_onehot.append(ud_features_dict[prontype_idx])
+                elif feat.startsWith("Case"):
+                    cases = feat.split("=")[1].split(",")
+                    for case in cases:
+                        case_idx = "Case=" + case
+                        word_feats_onehot.append(ud_features_dict[case_idx])
+                else:
+                    word_feats_onehot.append(ud_features_dict[feat])
             except:
-                print("No feat found: " + feat, flush=True)
                 continue
         document_state.morph_features[word_idx] = word_feats_onehot
         subtokens = tokenizer.tokenize(word)
@@ -354,6 +359,7 @@ def minimize_language(args):
     # minimize_partition('test', 'conllu', args, tokenizer)
 
     minimize_partition('dev', 'conllu', args, tokenizer)
+    minimize_partition('test', 'conllu', args, tokenizer)
     minimize_partition('train', 'conllu', args, tokenizer)
 
 
